ddd626d22bc485aefc745242d510471819f46ba3
Yin Song <songniy@gmail.com>
2021-03-11 22:15:35 -0500

    feature:automatic emails work. still need templates

--- client/components/emailer.js
+++ client/components/emailer.js
@@ -1,24 +1,76 @@
 const nodemailer = require('nodemailer')
+const {google} = require('googleapis')
+const {OAuth2} = google.auth
+const ejs = require('ejs')
+const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground'
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.GOOGLE_CLIENT_ID,
-    pass: 'SuperSecretPassword' // naturally, replace both with your real credentials or an application-specific password
+const {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_NO_REPLY_REFRESH_TOKEN,
+  GOOGLE_NO_REPLY_EMAIL
+} = process.env
+
+const MAILING_SERVICE_CLIENT_ID = GOOGLE_CLIENT_ID
+const MAILING_SERVICE_CLIENT_SECRET = GOOGLE_CLIENT_SECRET
+const MAILING_SERVICE_REFRESH_TOKEN = GOOGLE_NO_REPLY_REFRESH_TOKEN
+const SENDER_EMAIL_ADDRESS = GOOGLE_NO_REPLY_EMAIL
+
+console.log(
+  MAILING_SERVICE_CLIENT_ID,
+  MAILING_SERVICE_CLIENT_SECRET,
+  MAILING_SERVICE_REFRESH_TOKEN,
+  SENDER_EMAIL_ADDRESS
+)
+
+const Mailing = {}
+const oauth2Client = new OAuth2(
+  MAILING_SERVICE_CLIENT_ID,
+  MAILING_SERVICE_CLIENT_SECRET,
+  OAUTH_PLAYGROUND
+)
+
+const TEMPLATES = {
+  subscribe: {
+    fileName: 'subscribe.ejs',
+    subject: '[ABC Inc.] Welcome to ABC Inc.'
   }
-})
-
-const mailOptions = {
-  from: 'vindication@enron.com',
-  to: 'friendsofenron@gmail.com, enemiesofenron@gmail.com',
-  subject: 'Invoices due',
-  text: 'Dudes, we really need your money.'
 }
 
-transporter.sendMail(mailOptions, function(error, info) {
-  if (error) {
-    console.log(error)
-  } else {
-    console.log('Email sent: ' + info.response)
-  }
-})
+/**
+ * Send Email
+ */
+Mailing.sendEmail = data => {
+  oauth2Client.setCredentials({
+    refresh_token: MAILING_SERVICE_REFRESH_TOKEN
+  })
+  const accessToken = oauth2Client.getAccessToken()
+  const smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: SENDER_EMAIL_ADDRESS,
+      clientId: MAILING_SERVICE_CLIENT_ID,
+      clientSecret: MAILING_SERVICE_CLIENT_SECRET,
+      refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
+      accessToken
+    }
+  })
+  const filePath = `./client/components/template/${
+    TEMPLATES[data.template].fileName
+  }`
+  ejs.renderFile(filePath, data, {}, (e, content) => {
+    if (e) return e
+    const mailOptions = {
+      from: SENDER_EMAIL_ADDRESS,
+      to: data.email,
+      subject: TEMPLATES[data.template].subject,
+      html: content
+    }
+    smtpTransport.sendMail(mailOptions, (err, info) => {
+      if (err) return err
+      return info
+    })
+  })
+}
+module.exports = Mailing
---
+++ server/api/mailing.controller.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const Mailing = require('../../client/components/emailer')
+//import {SOME_THING_WENT_WRONG} from '../store/constant'
+//import {generateServerErrorCode} from '../store/utils'
+const mailingController = express.Router()
+/**
+ * POST/ User subscribe to App
+ */
+mailingController.post('/', (req, res, next) => {
+  try {
+    //req.query should be an object with {email,template}
+    //email could come from database.
+    Mailing.sendEmail(req.query)
+    res.status(200).json({message: 'email sent successfully'})
+  } catch (e) {
+    next(console.log('something went wrong res,event', res, 500, e))
+  }
+})
+module.exports = mailingController
