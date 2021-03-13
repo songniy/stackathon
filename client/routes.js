a90dbdcae0edc098e68068a83d2fdb3770b503a2
Yin Song <songniy@gmail.com>
2021-03-13 17:45:08 -0500

    fix: lower case react components-webpack error about casing

--- client/routes.js
+++ client/routes.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components/Index'
+import {Login, Signup, UserHome} from './components'
 import {me} from './store'
-import {Calendar} from './components/Calendar'
+import {Calendar} from './components/calendar'
 /**
  * COMPONENT
  */
