776fa49ee8ba545f1387ae5942aaf0a89c615d41
Yin Song <songniy@gmail.com>
2021-03-13 18:55:10 -0500

    heroku private key config var test

--- server/api/components/calendarAuth.js
+++ server/api/components/calendarAuth.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const {google} = require('googleapis')
+
+let privateKey_Private_Key = ''
+if (process.env.PGHOST === 'localhost') {
+  privateKey_Private_Key = process.env.PRIVATEKEY_PRIVATE_KEY
+} else {
+  privateKey_Private_Key = process.env.PRIVATEKEY_PRIVATE_KEY.replace(
+    /\\n/g,
+    '\n'
+  ).replace(/'/g, '')
+}
 const privateKey = {
   client_email: process.env.PRIVATEKEY_CLIENT_EMAIL,
-  private_key: process.env.PRIVATEKEY_PRIVATE_KEY
+  private_key: privateKey_Private_Key
 }
 //configure jwt client
 
