95d364ada1d6f37cdab0415fcb82e56fd1fab1f6
Yin Song <songniy@gmail.com>
2021-03-14 00:10:40 -0500

    again

--- script/preinstall.js
+++ script/preinstall.js
@@ -1,9 +1,13 @@
 const fs = require('fs')
 console.log(process.env)
 if (!process.env.TAVIS_OS_NAME) {
+  let googleAuth = {}
+  if (process.env.GOOGLE_CONFIG) {
+    googleAuth = process.env.GOOGLE_CONFIG
+  }
   fs.writeFile(
     '../GOOGLE_APPLICATION_CREDENTIALS.json',
-    process.env.GOOGLE_CONFIG,
+    googleAuth,
     (err) => {}
   )
 }
